7aacaa487d7ff86b6baac7e2e75e485e89c86dfd
Edson Smikatz <edsonsmikatz@hotmail.com>
2020-07-08 21:54:54 -0300

    read create update delete

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -40,6 +40,7 @@ class User extends Model{
 		}
 	}
 
+
 	public static function verifyLogin($inadmin = true)
 	{
 
@@ -62,7 +63,20 @@ class User extends Model{
 	public static function logout(){
 
 		$_SESSION[User::SESSION] = NULL;
+
 	}
+
+	public static function listAll(){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN  tb_persons b USING(idperson) ORDER BY b.desperson");
+
+
+	}
+
+
+
+
 }
 
 
